9463caa06b92a28950438c449b48fbfc231b78cc
stark-creator <76799356+stark-creator@users.noreply.github.com>
2022-11-15 18:52:16 -0500

    Add files via upload

---
+++ DjangoFileUpload/Core/views.py
@@ -0,0 +1,21 @@
+from . import models
+from django.shortcuts import render
+
+def uploadFile(request):
+    if request.method == "POST":
+        # Fetching the form data
+        fileTitle = request.POST["fileTitle"]
+        uploadedFile = request.FILES["uploadedFile"]
+
+        # Saving the information in the database
+        document = models.Document(
+            title = fileTitle,
+            uploadedFile = uploadedFile
+        )
+        document.save()
+
+    documents = models.Document.objects.all()
+
+    return render(request, "Core/upload-file.html", context = {
+        "files": documents
+    })
